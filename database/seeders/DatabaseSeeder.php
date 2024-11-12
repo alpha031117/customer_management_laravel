5375d7639178272ddbae598a38b4f6a53f13df4e
Alpha Chong <alphachongs@gmail.com>
2024-11-12 22:35:47 +0800

    update seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use App\Models\Customer;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -21,6 +20,8 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
-        Customer::factory(100)->create();
+        // Customer Seeder
+        $this->call(CustomerSeeder::class);
+        
     }
 }
